1135208b7fe2a9d9298aba7ed7e8df960706f396
Max <maximlev01234@gmail.com>
2021-05-05 19:34:56 +0300

    updated main screen (for coach)

--- components/main-screen/ListItemGroup.js
+++ components/main-screen/ListItemGroup.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Surface } from 'react-native-paper'
 
-export default function ListItemGroup({item}) {
+export default function ListItemGroup({ item }) {
     return (
-        <View>
-            <Text>{JSON.stringify(item)}</Text>
+        <View style={{ padding: 5, }}>
+            <Surface style={{ elevation: 4, borderRadius: 15, flexDirection: 'row' }}>
+                <View style={{ width: 20, backgroundColor: '#06224A', marginRight: 10, height: "100%", borderTopLeftRadius: 15, borderBottomLeftRadius: 15, }} />
+
+                <View style={{ paddingHorizontal: 10, paddingVertical: 10, justifyContent: 'space-between', }}>
+                    <Text style={{ fontWeight: 'bold', color: '#06224A', marginBottom: 10, }}>
+                        {item.title}
+                    </Text>
+                    <Text style={{ color: '#ccc' }}>
+                        {item.time}
+                    </Text>
+                </View>
+            </Surface>
         </View>
     )
 }
--- components/main-screen/MainScreen.js
+++ components/main-screen/MainScreen.js
@@ -1,32 +1,64 @@
 import React from 'react'
 import { Dimensions, FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native-paper';
 import ListItemGroup from './ListItemGroup';
 
 const imageWidth = Dimensions.get('window').width * 0.4
 const imageHeight = imageWidth;
 
+const scheludeItemsList = [
+    {
+        id: 1,
+        title: 'Група 2012 рік',
+        time: '12:30 - 13:45'
+    },
+    {
+        id: 2,
+        title: 'Група 2010 рік',
+        time: '14:15 - 15:30'
+    },
+    {
+        id: 3,
+        title: 'Група 2008 рік',
+        time: '18:00 - 19:45'
+    }
+]
+
 export default function MainScreen() {
 
-    const renderItem = ({item, index}) => {
-        return <ListItemGroup item={item} key={index}/>
+    const renderItem = ({ item, index }) => {
+        return <ListItemGroup item={item} key={JSON.stringify(item.id)} />
     }
 
     return (
-        <View style={{ marginTop: 30, marginLeft: 30 }}>
+        <View style={{ paddingTop: 30, marginHorizontal: 30, height: '100%' }}>
             <View style={{ flexDirection: 'row' }}>
                 <View style={{ width: '40%', }}>
                     <Text style={{ fontSize: 36, fontWeight: "800", marginTop: 40, }}>Привіт, Олег</Text>
-                    <Text style={{ fontSize: 10, color: '#444', marginTop: 20, }}>Сьогодні, 5 травня</Text>
+                    <Text style={{ fontSize: 14, color: '#444', marginTop: 20, }}>Сьогодні, 5 травня</Text>
                 </View>
                 <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 20, }}>
                     <Image style={{ height: imageWidth, width: imageHeight, }} source={require("../../photos/main_screen_top2.png")} />
                 </View>
             </View>
-            <Text style={{ marginTop: 40, fontSize: 20, }}>Розклад занять</Text>
+            <Text style={{ marginTop: 40, fontSize: 20, fontWeight: 'bold', color: '#06224A', marginBottom: 20 }}>Розклад занять</Text>
             <FlatList
-                data={[1, 2, 3, 4]}
+                data={scheludeItemsList}
                 renderItem={renderItem}
+                keyExtractor={(item) => JSON.stringify(item.id)}
+                contentContainerStyle={{ flex: 1 }}
             />
+
+            <View style={{
+                marginBottom: 20,
+                flexDirection: 'row'
+            }}
+            >
+                <Text>
+                    Приєднатися до спорядження
+                </Text>
+                <Button mode='contained'>Next</Button>
+            </View>
         </View>
     )
 }
