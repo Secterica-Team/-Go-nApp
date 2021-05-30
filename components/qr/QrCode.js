f63889b230bc202de91a23adacb937c48a564d40
max4nik <max.nik3032@gmail.com>
2021-05-30 22:31:58 +0300

    added add gates screen

--- components/qr/QrCode.js
+++ components/qr/QrCode.js
@@ -1,53 +1,167 @@
 import { useNavigation } from "@react-navigation/core";
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import {
-    FlatList,
-    ImageBackground,
-    StyleSheet,
-    Text,
-    View,
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
 } from "react-native";
-import { Appbar, List } from "react-native-paper";
+import {
+  Appbar,
+  List,
+  Card,
+  Title,
+  Paragraph,
+  Button,
+  TextInput,
+} from "react-native-paper";
 import ComponentWithButtonTextAndBackgroundImage from "../main-screen/ComponentWithButtonTextAndBackgroundImage";
 
-
-
-const gateways = [
-    { id: 1, distance: 10 },
-    { id: 2, distance: 30 },
-    { id: 3, distance: 50 },
-    { id: 4, distance: 75 },
-    { id: 5, distance: 100 },
-];
-
-const bg1 = require("../../photos/backgr/bg.png");
-const bg2 = require("../../photos/backgr/bg_2.png");
+const Gate = ({ item }) => (
+  <View
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#85BAF3",
+      height: 40,
+      marginBottom: 15,
+      borderRadius: 10,
+    }}
+  >
+    <Text style={{ fontSize: 14, fontWeight: "500", color: "#FFFF" }}>
+      {item.distance} метрів
+    </Text>
+  </View>
+);
 
 export default function QrCode() {
-    const navigation = useNavigation();
-
-    return (<View>
-            <View style={{ height: "100%" }}>
-                <Appbar.Header theme={{ colors: { primary: "white" } }}>
-                    <Appbar.Action
-                        onPress={() => {
-                            navigation.goBack();
-                        }}
-                        icon={"chevron-left"}
-                        size={30}
-                        color={"#888"}
-                    />
-                    <Appbar.Content title="Підключення" />
-                    <Appbar.Action icon="dots-vertical" onPress={() => {}} color={"#888"} />
-                </Appbar.Header>
-
-
+  const navigation = useNavigation();
+  const [distance, setDistance] = useState();
+  const [gates, setGates] = useState();
+  const current_gates = useSelector((state) => state.gates);
+  const renderItem = ({ item }) => {
+    return <Gate item={item} />;
+  };
+  return (
+    <View>
+      <View style={{ height: "100%", backgroundColor: "white" }}>
+        <Appbar.Header theme={{ colors: { primary: "white" } }}>
+          <Appbar.Action
+            onPress={() => {
+              navigation.goBack();
+            }}
+            icon={"chevron-left"}
+            size={30}
+            color={"#888"}
+          />
+          <Appbar.Content
+            title={<Title style={{ fontSize: 22 }}>Підключення</Title>}
+          />
+          <Appbar.Action
+            icon="dots-vertical"
+            onPress={() => {}}
+            color={"#888"}
+          />
+        </Appbar.Header>
+        <Card style={styles.distanceContainer}>
+          <Card.Content>
+            <Title style={styles.inputTitle}>Введіть відстань</Title>
+            <Paragraph style={styles.inputSubtitle}>
+              Задайте параметри, щоб встановити розподіл шляху, після цього
+              відскануйте QR код пристрою
+            </Paragraph>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Наприклад, 0"
+                mode="outlined"
+                value={distance}
+                onChangeText={(distance) => setDistance(distance)}
+                outlineColor="#85BAF3"
+              />
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("DeviceQrScanner", {
+                    entered_distance: distance,
+                  });
+                }}
+              >
+                <Image
+                  style={styles.addDistanceImg}
+                  source={require("../../photos/add_distance.png")}
+                />
+              </TouchableOpacity>
             </View>
+          </Card.Content>
+        </Card>
+        <View style={styles.devicesContainer}>
+          <Title style={styles.inputTitle}>Ви вже додали...</Title>
+          <Paragraph
+            style={{
+              fontSize: 12,
+              color: "rgba(0, 0, 0, 0.54)",
+              marginBottom: 14,
+            }}
+          >
+            Ви можете переглядати та змінювати ваші девайси перед завершенням
+            додавання
+          </Paragraph>
+          <FlatList
+            data={Array.from(current_gates.gates)}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            style={{ maxHeight: 175 }}
+          />
+        </View>
+      </View>
     </View>
-
-
-
-    );
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  distanceContainer: {
+    marginTop: "10%",
+    width: "95%",
+    alignSelf: "center",
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 4,
+      height: 3,
+    },
+    shadowOpacity: 0.35,
+    shadowRadius: 6,
+
+    elevation: 7,
+  },
+  devicesContainer: {
+    marginTop: "8%",
+    width: "90%",
+    alignSelf: "center",
+  },
+  input: {
+    marginTop: "10%",
+    marginBottom: "5%",
+    width: "75%",
+    borderColor: "#85BAF3",
+  },
+  inputTitle: {
+    fontSize: 16,
+  },
+  inputSubtitle: {
+    fontSize: 12,
+    color: "rgba(0, 0, 0, 0.54)",
+  },
+  inputContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  addDistanceImg: { width: 50, height: 50, marginTop: 15, marginRight: 5 },
+});
